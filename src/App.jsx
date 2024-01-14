6304ac7995bd90183039cca09b659e60354ba33d
mraiyuu <munenedenis618@gmail.com>
2024-01-15 00:29:50 +0300

    hooks with objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import HookCount from "./components/HookCount";
 import HookCountTwo from "./components/HookCountTwo";
+import HookCounterThree from "./components/HookCounterThree";
 
 const App = () => {
   return (
     <div className="w-screen h-full flex flex-row items-center justify-center gap-5 overflow-hidden mt-[24px]">
-      <HookCountTwo />
+     <HookCounterThree />
     </div>
   );
 };
